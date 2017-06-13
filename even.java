5ba72d356cc103643dfa851deb40bcb861c575cd
sangeetha143 <sangeethamadhi2@gmail.com>
2017-06-13 10:01:44 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class kannan
+{
+public static void main(String a[])
+{
+int n;
+Scanner s=new Scanner (System.in);
+n=s.nextInt();
+if(n%2==0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
